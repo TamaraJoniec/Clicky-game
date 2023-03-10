1e70c5283618f88e4fbd4953fd294a2e7e062489
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-03-10 16:59:51 +0000

    added usestate and reshuffle on click function

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { useState } from 'react';
 
 // create function that returns a card component 
 function Card({image}) {
@@ -16,21 +17,36 @@ function Card({image}) {
 }
 
 function App() {
-  const cards = [
-    { image: 'https://images.pexels.com/photos/50577/hedgehog-animal-baby-cute-50577.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/735423/pexels-photo-735423.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/162140/duckling-birds-yellow-fluffy-162140.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/69372/pexels-photo-69372.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/1464518/pexels-photo-1464518.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/157465/pexels-photo-157465.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/1525606/pexels-photo-1525606.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/2883510/pexels-photo-2883510.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/6337840/pexels-photo-6337840.jpeg?auto=compress&cs=tinysrgb&w=400' },
-    { image: 'https://images.pexels.com/photos/802112/pexels-photo-802112.jpeg?auto=compress&cs=tinysrgb&w=400' }    
-  ];
+  const [cards, setCards] = useState([
+    { id: 1, image: 'https://images.pexels.com/photos/50577/hedgehog-animal-baby-cute-50577.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 2, image: 'https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 3, image: 'https://images.pexels.com/photos/735423/pexels-photo-735423.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 4, image: 'https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 5, image: 'https://images.pexels.com/photos/162140/duckling-birds-yellow-fluffy-162140.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 6, image: 'https://images.pexels.com/photos/69372/pexels-photo-69372.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 7, image: 'https://images.pexels.com/photos/1464518/pexels-photo-1464518.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 8, image: 'https://images.pexels.com/photos/157465/pexels-photo-157465.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 9, image: 'https://images.pexels.com/photos/1525606/pexels-photo-1525606.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 10, image: 'https://images.pexels.com/photos/2883510/pexels-photo-2883510.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 11, image: 'https://images.pexels.com/photos/6337840/pexels-photo-6337840.jpeg?auto=compress&cs=tinysrgb&w=400' },
+    { id: 12, image: 'https://images.pexels.com/photos/802112/pexels-photo-802112.jpeg?auto=compress&cs=tinysrgb&w=400' }
+  ]);
 
+  function handleCardClick(id) {
+    const shuffledCards = shuffleArray(cards);
+    setCards(shuffledCards);
+  }
+
+  function shuffleArray(array) {
+    // Create a new copy of the array to avoid mutating the original
+    const newArray = [...array];
+    // Shuffle the new array
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  }
   return (
     <div className="container">
       <h1 className="text-center mb-4">Memory Game</h1>
