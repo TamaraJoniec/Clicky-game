120510e81c543e5e872981799fbee60900651660
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-03-10 16:51:24 +0000

    reversed git history

---
+++ memory-game/src/cards.js
@@ -0,0 +1,55 @@
+import './App.css';
+import React from 'react';
+
+const cards = [
+      {
+        "id": 1,
+        "image": "https://images.pexels.com/photos/50577/hedgehog-animal-baby-cute-50577.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 2,
+        "image": "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 3,
+        "image": "https://images.pexels.com/photos/735423/pexels-photo-735423.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 4,
+        "image": "https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 5,
+        "image": "https://images.pexels.com/photos/162140/duckling-birds-yellow-fluffy-162140.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 6,
+        "image": "https://images.pexels.com/photos/69372/pexels-photo-69372.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 7,
+        "image": "https://images.pexels.com/photos/1464518/pexels-photo-1464518.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 8,
+        "image": "https://images.pexels.com/photos/157465/pexels-photo-157465.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 9,
+        "image": "https://images.pexels.com/photos/1525606/pexels-photo-1525606.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 10,
+        "image": "https://images.pexels.com/photos/2883510/pexels-photo-2883510.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 11,
+        "image": "https://images.pexels.com/photos/6337840/pexels-photo-6337840.jpeg?auto=compress&cs=tinysrgb&w=400"
+      },
+      {
+        "id": 12,
+        "image": "https://images.pexels.com/photos/802112/pexels-photo-802112.jpeg?auto=compress&cs=tinysrgb&w=400"
+      }
+    ]
+  
+  
